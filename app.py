270c4c9ddfa7b2f8556958b3b5169729c054a38d
Courtney Gomez <courtneygomez@Courtneys-MBP.lan>
2022-06-23 17:46:26 -0500

    added dependencies

--- app.py
+++ app.py
@@ -1,5 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for, redirect, flash
 import pandas as pd
+
+from flask_sqlalchemy import SQLAlchemy
+import os
+
+from sqlalchemy.sql import func
+
+from sqlalchemy import create_engine, func, Column, Integer, String, Date
+
+from flask_marshmallow import Marshmallow
+from marshmallow import Schema, fields
+
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
 
 app = Flask(__name__)
 
