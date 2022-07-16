2ce29e4078cd279f792333d9de08fccb1f3e4bb7
Courtney Gomez <courtneygomez@Courtneys-MacBook-Pro.local>
2022-07-16 18:54:27 -0500

    changed uri

--- config.py
+++ config.py
@@ -4,4 +4,4 @@ port=5432
 db='d8mderhrcoiklo'
 username = 'JHeath'
 app_password = 'Parish!'
-uri='postgres://dfgyfkfguwlajn:4a04b52da15210f7e67513a97ac3e21d0a8f1b05e927dff75381c8af4281f839@ec2-52-206-182-219.compute-1.amazonaws.com:5432/d8mderhrcoiklo'
+uri='postgresql://dfgyfkfguwlajn:4a04b52da15210f7e67513a97ac3e21d0a8f1b05e927dff75381c8af4281f839@ec2-52-206-182-219.compute-1.amazonaws.com:5432/d8mderhrcoiklo'
