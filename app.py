f20de221734e649f5ed46e724d70d9af6696a8a1
Courtney Gomez <courtneygomez@Courtneys-MacBook-Pro.local>
2022-08-20 17:32:52 -0500

    added data and functions

--- app.py
+++ app.py
@@ -1,21 +1,22 @@
 from flask import Flask, render_template, request, url_for, redirect, flash
 import pandas as pd
+from functions import Game_Data
 
 
 app = Flask(__name__)
 
 #connect to database
 
+data=Game_Data
 
 
-
-@app.route('/')
+@app.route('/logout')
 def index():
-    return render_template('base.html')
+    return data.full_data()
 
 @app.route('/home')
 def home():
-    return "This is the home page"
+    return render_template('home.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ functions.py
@@ -0,0 +1,22 @@
+from sqlalchemy import create_engine
+import pandas as pd
+
+
+##create database connection to existing data
+engine=create_engine('sqlite:///parish_data.db', echo=True)
+sqlite_connection = engine.connect()
+
+file = pd.read_excel('Aledo_vs_Timberview_102521.xlsx')
+
+sqlite_table = "Football"
+file.to_sql(sqlite_table, sqlite_connection, if_exists='append', index=False)
+
+full_df = pd.read_sql('SELECT * from Football', sqlite_connection)
+
+class Game_Data():
+    def full_data():
+        return full_df
+    
+    def queried_data(qtr):
+        queried_df = full_df[full_df.QTR==qtr]
+        return queried_df
